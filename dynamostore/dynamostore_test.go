e65787efaa3b614c5d728e77d16afd50a7208d27
TANABE Ken-ichi <nabeken@tknetworks.org>
2016-02-20 16:50:18 +0900

    test: cleanup...

--- dynamostore/dynamostore_test.go
+++ dynamostore/dynamostore_test.go
@@ -80,14 +80,10 @@ func TestStore(t *testing.T) {
 		t.Skip("Do not run integration tests unless DYNAMOSTORE_INTEG_TEST is set")
 	}
 
-	var req *http.Request
-	var rsp *httptest.ResponseRecorder
-	var hdr http.Header
 	var err error
 	var ok bool
 	var cookies []string
 	var session *sessions.Session
-	var flashes []interface{}
 
 	// Copyright 2012 The Gorilla Authors. All rights reserved.
 	// Use of this source code is governed by a BSD-style
@@ -108,127 +104,132 @@ func TestStore(t *testing.T) {
 		TableName: aws.String(dummyTableName),
 	})
 
-	// Round 1 ----------------------------------------------------------------
 	store := New(dynamodbClient, dummyTableName, []byte("secret-key"))
 
-	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
-	rsp = httptest.NewRecorder()
-	// Get a session.
-	if session, err = store.Get(req, "session-key"); err != nil {
-		t.Fatalf("Error getting session: %v", err)
-	}
-	// Get a flash.
-	flashes = session.Flashes()
-	if len(flashes) != 0 {
-		t.Errorf("Expected empty flashes; Got %v", flashes)
-	}
-	// Add some flashes.
-	session.AddFlash("foo")
-	session.AddFlash("bar")
-	// Custom key.
-	session.AddFlash("baz", "custom_key")
-	// Save.
-	if err = sessions.Save(req, rsp); err != nil {
-		t.Fatalf("Error saving session: %v", err)
-	}
-	hdr = rsp.Header()
-	cookies, ok = hdr["Set-Cookie"]
-	if !ok || len(cookies) != 1 {
-		t.Fatalf("No cookies. Header:", hdr)
+	// Round 1 ----------------------------------------------------------------
+	{
+		req, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+		rsp := httptest.NewRecorder()
+		// Get a session.
+		if session, err = store.Get(req, "session-key"); err != nil {
+			t.Fatalf("Error getting session: %v", err)
+		}
+		// Get a flash.
+		flashes := session.Flashes()
+		if len(flashes) != 0 {
+			t.Errorf("Expected empty flashes; Got %v", flashes)
+		}
+		// Add some flashes.
+		session.AddFlash("foo")
+		session.AddFlash("bar")
+		// Custom key.
+		session.AddFlash("baz", "custom_key")
+		// Save.
+		if err = sessions.Save(req, rsp); err != nil {
+			t.Fatalf("Error saving session: %v", err)
+		}
+		hdr := rsp.Header()
+		cookies, ok = hdr["Set-Cookie"]
+		if !ok || len(cookies) != 1 {
+			t.Fatalf("No cookies. Header:", hdr)
+		}
 	}
 
 	// Round 2 ----------------------------------------------------------------
+	{
+		req, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+		req.Header.Add("Cookie", cookies[0])
+		rsp := httptest.NewRecorder()
+		// Get a session.
+		if session, err = store.Get(req, "session-key"); err != nil {
+			t.Fatalf("Error getting session: %v", err)
+		}
+		// Check all saved values.
+		flashes := session.Flashes()
+		if len(flashes) != 2 {
+			t.Fatalf("Expected flashes; Got %v", flashes)
+		}
+		if flashes[0] != "foo" || flashes[1] != "bar" {
+			t.Errorf("Expected foo,bar; Got %v", flashes)
+		}
+		flashes = session.Flashes()
+		if len(flashes) != 0 {
+			t.Errorf("Expected dumped flashes; Got %v", flashes)
+		}
+		// Custom key.
+		flashes = session.Flashes("custom_key")
+		if len(flashes) != 1 {
+			t.Errorf("Expected flashes; Got %v", flashes)
 
-	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
-	req.Header.Add("Cookie", cookies[0])
-	rsp = httptest.NewRecorder()
-	// Get a session.
-	if session, err = store.Get(req, "session-key"); err != nil {
-		t.Fatalf("Error getting session: %v", err)
-	}
-	// Check all saved values.
-	flashes = session.Flashes()
-	if len(flashes) != 2 {
-		t.Fatalf("Expected flashes; Got %v", flashes)
-	}
-	if flashes[0] != "foo" || flashes[1] != "bar" {
-		t.Errorf("Expected foo,bar; Got %v", flashes)
-	}
-	flashes = session.Flashes()
-	if len(flashes) != 0 {
-		t.Errorf("Expected dumped flashes; Got %v", flashes)
-	}
-	// Custom key.
-	flashes = session.Flashes("custom_key")
-	if len(flashes) != 1 {
-		t.Errorf("Expected flashes; Got %v", flashes)
+		} else if flashes[0] != "baz" {
+			t.Errorf("Expected baz; Got %v", flashes)
+		}
+		flashes = session.Flashes("custom_key")
+		if len(flashes) != 0 {
+			t.Errorf("Expected dumped flashes; Got %v", flashes)
+		}
 
-	} else if flashes[0] != "baz" {
-		t.Errorf("Expected baz; Got %v", flashes)
-	}
-	flashes = session.Flashes("custom_key")
-	if len(flashes) != 0 {
-		t.Errorf("Expected dumped flashes; Got %v", flashes)
-	}
-
-	session.Options.MaxAge = -1
-	// Save.
-	if err = sessions.Save(req, rsp); err != nil {
-		t.Fatalf("Error saving session: %v", err)
+		session.Options.MaxAge = -1
+		// Save.
+		if err = sessions.Save(req, rsp); err != nil {
+			t.Fatalf("Error saving session: %v", err)
+		}
 	}
 
 	// Round 3 ----------------------------------------------------------------
 	// Custom type
-
-	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
-	rsp = httptest.NewRecorder()
-	// Get a session.
-	if session, err = store.Get(req, "session-key"); err != nil {
-		t.Fatalf("Error getting session: %v", err)
-	}
-	// Get a flash.
-	flashes = session.Flashes()
-	if len(flashes) != 0 {
-		t.Errorf("Expected empty flashes; Got %v", flashes)
-	}
-	// Add some flashes.
-	session.AddFlash(&FlashMessage{42, "foo"})
-	// Save.
-	if err = sessions.Save(req, rsp); err != nil {
-		t.Fatalf("Error saving session: %v", err)
-	}
-	hdr = rsp.Header()
-	cookies, ok = hdr["Set-Cookie"]
-	if !ok || len(cookies) != 1 {
-		t.Fatalf("No cookies. Header:", hdr)
+	{
+		req, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+		rsp := httptest.NewRecorder()
+		// Get a session.
+		if session, err = store.Get(req, "session-key"); err != nil {
+			t.Fatalf("Error getting session: %v", err)
+		}
+		// Get a flash.
+		flashes := session.Flashes()
+		if len(flashes) != 0 {
+			t.Errorf("Expected empty flashes; Got %v", flashes)
+		}
+		// Add some flashes.
+		session.AddFlash(&FlashMessage{42, "foo"})
+		// Save.
+		if err = sessions.Save(req, rsp); err != nil {
+			t.Fatalf("Error saving session: %v", err)
+		}
+		hdr := rsp.Header()
+		cookies, ok = hdr["Set-Cookie"]
+		if !ok || len(cookies) != 1 {
+			t.Fatalf("No cookies. Header:", hdr)
+		}
 	}
 
 	// Round 4 ----------------------------------------------------------------
 	// Custom type
+	{
+		req, _ := http.NewRequest("GET", "http://localhost:8080/", nil)
+		req.Header.Add("Cookie", cookies[0])
+		rsp := httptest.NewRecorder()
+		// Get a session.
+		if session, err = store.Get(req, "session-key"); err != nil {
+			t.Fatalf("Error getting session: %v", err)
+		}
 
-	req, _ = http.NewRequest("GET", "http://localhost:8080/", nil)
-	req.Header.Add("Cookie", cookies[0])
-	rsp = httptest.NewRecorder()
-	// Get a session.
-	if session, err = store.Get(req, "session-key"); err != nil {
-		t.Fatalf("Error getting session: %v", err)
-	}
+		// Check all saved values.
+		flashes := session.Flashes()
+		if len(flashes) != 1 {
+			t.Fatalf("Expected flashes; Got %v", flashes)
+		}
+		custom := flashes[0].(FlashMessage)
+		if custom.Type != 42 || custom.Message != "foo" {
+			t.Errorf("Expected %#v, got %#v", FlashMessage{42, "foo"}, custom)
+		}
 
-	// Check all saved values.
-	flashes = session.Flashes()
-	if len(flashes) != 1 {
-		t.Fatalf("Expected flashes; Got %v", flashes)
-	}
-	custom := flashes[0].(FlashMessage)
-	if custom.Type != 42 || custom.Message != "foo" {
-		t.Errorf("Expected %#v, got %#v", FlashMessage{42, "foo"}, custom)
-	}
-
-	// Delete session.
-	session.Options.MaxAge = -1
-	// Save.
-	if err = sessions.Save(req, rsp); err != nil {
-		t.Fatalf("Error saving session: %v", err)
+		// Delete session.
+		session.Options.MaxAge = -1
+		// Save.
+		if err = sessions.Save(req, rsp); err != nil {
+			t.Fatalf("Error saving session: %v", err)
+		}
 	}
 }
 
